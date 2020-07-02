815dc1c714f21086d0baf0944c631aa81d672e36
longlong lu <qq836482561@gmail.com>
2020-07-02 18:55:19 -0400

    buildi

---
+++ build/precache-manifest.4ae0d9b9254833fc4016d9463110a303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67907db4608bfdf06af6c7c967064dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ea22c72e18916ea429",
+    "url": "/static/js/2.60113508.chunk.js"
+  },
+  {
+    "revision": "5ced2f2ce7b79285485a360b83230d1b",
+    "url": "/static/js/2.60113508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cdf6b8fa78c5a9a532",
+    "url": "/static/js/main.8d9100d7.chunk.js"
+  },
+  {
+    "revision": "5852bed5f61792be237a",
+    "url": "/static/js/runtime-main.37a316c0.js"
+  },
+  {
+    "revision": "dad7a3545e08683edf58342f4f389038",
+    "url": "/static/media/saito_asuka.dad7a354.jpg"
+  }
+]);
