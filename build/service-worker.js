cbdc96cd91753edeeb6d1f80781b3227fcecdb2f
longlong lu <qq836482561@gmail.com>
2020-07-02 21:37:03 -0400

    buildi

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9afe5c7ba88ce95197e2a2fc51272cd.js"
+  "/precache-manifest.a0e9bc16262efcc69f87717609d23e7c.js"
 );
 
 self.addEventListener('message', (event) => {
